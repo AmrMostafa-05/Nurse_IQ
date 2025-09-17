2fb40d633c410aa82997c54f3b7e93455bb4c587
DAHY34 <ada7y34@gmail.com>
2025-09-17 03:32:36 +0300

    Done

--- Nurse_IQ/Data/Config/OfferConfiguration.cs
+++ Nurse_IQ/Data/Config/OfferConfiguration.cs
@@ -32,7 +32,6 @@ namespace Nurse_IQ.Data.Config
 
             builder.Property(o => o.DiscountPercentage)
                    .IsRequired();
-            // Computed in SQL
             builder.Property(o => o.DiscountPrice)
                    .HasColumnType("decimal(18, 2)")
                    .HasComputedColumnSql("[OriginalPrice] * [discountPercentage] / 100", stored: true);
